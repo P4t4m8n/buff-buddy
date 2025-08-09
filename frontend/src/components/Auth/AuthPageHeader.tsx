cde7f4d42f55ae16bf5c1ac5daa59c64371fd4dd
P4t4m8n <michaelieran@gmail.com>
2025-08-09 11:21:49 +0300

    Icons update for  PWA

--- frontend/src/components/Auth/AuthPageHeader.tsx
+++ frontend/src/components/Auth/AuthPageHeader.tsx
@@ -1,11 +1,10 @@
-import IconLogo from "../UI/Icons/IconLogo";
 
 //TODO??Placeholder for future header content
 export default function AuthPageHeader() {
   return (
     <header className="text-center grid gap-1 justify-items-center ">
-      <div className=" bg-white rounded-full p-4 flex items-center justify-center shadow">
-        <IconLogo className="w-12 h-12" />
+      <div className=" bg-main-orange  rounded-full p-4 flex items-center justify-center shadow">
+        <img className="w-36 aspect-square" src="assets/logo.svg"></img>
       </div>
       <h1 className="text-3xl font-bold text-white ">Buff Buddy</h1>
       <p className="text-white/80">Your fitness journey starts here</p>
