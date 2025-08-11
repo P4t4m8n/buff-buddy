607a83e04006dcb7b096441c5583ead2cd312acc
p4t4m8n <michaelieran@gmail.com>
2025-08-11 09:36:36 +0300

    Gen Prisma client

--- backend/prisma/generated/prisma/index.js
+++ backend/prisma/generated/prisma/index.js
@@ -425,7 +425,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Users\\micha\\Desktop\\Projects\\buff-buddy\\backend\\prisma\\generated\\prisma",
+      "value": "C:\\Users\\micha\\Desktop\\projects\\buff-buddy\\backend\\prisma\\generated\\prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -439,7 +439,7 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Users\\micha\\Desktop\\Projects\\buff-buddy\\backend\\prisma\\schema.prisma",
+    "sourceFilePath": "C:\\Users\\micha\\Desktop\\projects\\buff-buddy\\backend\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
@@ -453,7 +453,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
