a0b2eaf9618a9ef5ab799f341495dcbf7ca95e2a
P4t4m8n <michaelieran@gmail.com>
2025-08-29 13:51:10 +0300

    Fixed label click

--- frontend/src/components/UI/Form/Label.tsx
+++ frontend/src/components/UI/Form/Label.tsx
@@ -5,7 +5,7 @@ const moveUpEffect = `absolute bg-inherit rounded z-10 transition-all duration-3
                      peer-focus:-top-0 peer-focus:text-xs peer-focus:px-3 peer-focus:text-black peer-focus:font-semibold  peer-focus:bg-main-orange
                      peer-[:not(:placeholder-shown)]:-top-0 peer-[:not(:placeholder-shown)]:text-xs peer-[:not(:placeholder-shown)]:px-3
                      peer-[:not(:placeholder-shown)]:text-black peer-[:not(:placeholder-shown)]:bg-main-orange
-                      peer-[:not(:placeholder-shown)]:font-semibold `;
+                      peer-[:not(:placeholder-shown)]:font-semibold pointer-events-none `;
 
 interface Props extends React.LabelHTMLAttributes<HTMLLabelElement> {
   children?: React.ReactNode;
