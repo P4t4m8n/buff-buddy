33a4b238eff60295e1595c6eb8202e10b51d37dd
P4t4m8n <michaelieran@gmail.com>
2025-08-29 13:51:44 +0300

    Remove default name

--- frontend/src/components/UI/Form/IsActiveInput.tsx
+++ frontend/src/components/UI/Form/IsActiveInput.tsx
@@ -1,8 +1,14 @@
 import { twMerge } from "tailwind-merge";
-import Input from "./Input";
-import Label from "./Label";
+
 import { appUtil } from "../../../utils/app.util";
 
+import Input from "./Input";
+import Label from "./Label";
+
+const DEFAULT_AFTER_CONTENT_TEXT = {
+  active: "Active",
+  inactive: "Inactive",
+};
 interface IIsActiveInputProps {
   handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   isActive: boolean;
@@ -13,20 +19,16 @@ interface IIsActiveInputProps {
   };
 }
 
-const DEFAULT_AFTER_CONTENT_TEXT = {
-  active: "Active",
-  inactive: "Inactive",
-};
 export default function IsActiveInput({
   handleInputChange,
   isActive,
   afterContentText,
-  inputName = "isActive",
+  inputName,
 }: IIsActiveInputProps) {
   const safeAfter = afterContentText ?? DEFAULT_AFTER_CONTENT_TEXT;
 
   const mainClass =
-    " relative w-24  h-10 rounded-full bg-error-red cursor-pointer transition-colors duration-[400ms] block peer-checked:bg-success-green";
+    " relative w-24 h-10 rounded-full bg-error-red cursor-pointer transition-colors duration-[400ms] block peer-checked:bg-success-green";
 
   const before = `before:absolute before:left-1 before:top-1/2 before:h-6 before:w-6 before:translate-x-16 before:-translate-y-1/2
           before:rounded-full before:bg-white before:shadow-sm before:transition-transform before:duration-[400ms] before:content-['']
@@ -34,8 +36,10 @@ export default function IsActiveInput({
 
   const after = `after:absolute after:left-4 after:top-1/2 after:-translate-y-1/2 after:text-xs after:font-bold after:text-white
           after:transition-all after:duration-200  peer-checked:after:left-auto peer-checked:after:right-4 `;
+
   const afterAttrClass =
     "after:content-[attr(data-after-inactive)] peer-checked:after:content-[attr(data-after-active)]";
+
   const labelClass = twMerge(mainClass, before, after, afterAttrClass);
 
   const idName = inputName + appUtil.getTempId();
