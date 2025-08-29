4cc3730f94daa3e67aaef34f011c8fbadb7da620
P4t4m8n <michaelieran@gmail.com>
2025-08-29 13:52:26 +0300

    Create form util with onChange function

---
+++ frontend/src/utils/form.util.ts
@@ -0,0 +1,34 @@
+const handleInputChange = <T>(
+  e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  setStateToEdit: React.Dispatch<React.SetStateAction<T | null>>
+) => {
+  e.stopPropagation();
+  const target = e.target as HTMLInputElement;
+  const { name, value, type, checked } = target;
+  console.log("🚀 ~ handleInputChange ~ name:", name)
+
+  let newVal: boolean | string | number | null;
+  switch (type) {
+    case "checkbox":
+      newVal = checked;
+      break;
+    case "number":
+      newVal = parseFloat(value);
+      break;
+    default:
+      newVal = value;
+      break;
+  }
+
+  setStateToEdit((prev) => {
+    if (!prev) return null;
+    return {
+      ...prev,
+      [name]: newVal,
+    };
+  });
+};
+
+export const formUtils = {
+  handleInputChange,
+};
